a00df72ca8a67b2e346252748c66d35a09b60a4d
CALEXCO <alexortegaleonato@gmail.com>
2024-11-30 15:48:02 +0100

    Parse CLI argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the project
+    project_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
+    
 }
